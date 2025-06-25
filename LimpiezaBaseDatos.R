1181e4e0b43411e81eb5a0c087f7caaf2ecd63d5
Andrey Prado <joseandrey.prado@ucr.ac.cr>
2025-06-25 15:32:55 -0600

    feat: arreglo de fechas

--- LimpiezaBaseDatos.R
+++ LimpiezaBaseDatos.R
@@ -12,131 +12,117 @@ view(base)
 # Selección de Columnas para el estudio
 
 base <- base %>% 
-  select(sitio, ubi_muestra = ubicación, cuerpo, fecha,hora, profundidad, secchi, temperatura, salinidad,
-         oxigeno, solidos_dis = TDS , conduc, conduc_abs =conduc_ABS,
-         resist, PPWO, presion, sat_oxigen, latitud, longitud,
-         analisis_agua1, ph, ph1, 
-         fosfatos, silicatos, amonio, nitritos, nitratos, filtros_chla,
-         chla_agua, faopigmentos, filtros_matsuspension, matsuspension,
-         , alcali_parcial, alcali_total, carbonatos, zinc, cobre,  ca2, mg2, na, k,cl,
-         SO42, dbof, dboj, dqof, dqoj)
-
-# Limpieza de columnas y asignación de NA's
-
-base <- base %>% 
-  mutate(
-    ubi_muestra = case_when(
-      ubi_muestra == 1 ~ 'superficie',
-      ubi_muestra == 2 ~ 'fondo',
-      TRUE ~ NA 
-    ),
-    cuerpo = case_when(
-      cuerpo == 1 ~ 'dulce',
-      cuerpo == 2 ~ 'salado',
-      TRUE ~ NA
-    ),
-    analisis_agua1 = case_when(
-      analisis_agua1 == 0 ~ 'no',
-      analisis_agua1 == 1 ~ 'si',
-      TRUE ~ NA
-    )
+  select(
+    # Variables Generales 
+    sitio, ubi_muestra = ubicación, cuerpo, latitudf, latitudj, longitudf, longitudj,
+    # Variables del muestreo
+    fecha, fecharecolectaf, fecharecolectaj,
+    # Variables del sitio
+    profundidad, salinidad, oxigeno, solid_dis = TDS, conduc, resist, presion, sat_oxigen,
+    # Variables del agua y residuos en Febrero
+    tempaguaf, tempairef, colifecalf, ecolif, enterococof,
+    # Variablaes del agua y residuos en Julio
+    tempaguaj, tempairej, colifecalj, ecolij, enterococoj,
+    # Variables generales del agua y sedimentos
+    ph, fosfatos, silicatos, amonio, nitritos, nitratos, chla_agua, faopigmentos, matsuspension, 
+    alcali_total, dureza, carbonatos,
+    # Variables acerca de la concentración de químicos en el agua
+    zinc, cobre, plomo, cadmio, arsenico, manganeso, ca2, mg2, na, k, cl, SO42, 
+    # Índices sobre la calidad del agua
+    dbof, dboj, dqof, dqoj
   )
+
+
+# Categóricas
+
+base <- base %>% mutate(
+  ubi_muestra = case_when(
+    ubi_muestra == "1" ~ "Superficie",
+    ubi_muestra == "2" ~ "Fondo",
+    ubi_muestra == "-1" ~ NA,
+    TRUE ~ NA
+  ),
+  cuerpo = case_when(
+    cuerpo == "1" ~ "Salado",
+    cuerpo == "2" ~ "Dulce",
+    TRUE ~ NA
+  ),
+  fecha = case_when(
+    str_detect(fecha, "^99/99") ~ NA,
+    str_detect(fecha, "-1") ~ NA,
+    fecha == "02dec2019" ~ "02/12/2019",
+    fecha == "02nov2019" ~ "02/11/2019",
+    fecha == "11feb2019" ~ "11/02/2019",
+    TRUE ~ as.character(fecha)
+  ),
+  fecharecolectaf = case_when(
+    str_detect(fecharecolectaf, "^99/99") ~ NA,
+    str_detect(fecharecolectaf, "00/00/00") ~ NA,
+    str_detect(fecharecolectaf, "-1") ~ NA,
+    TRUE ~ as.character(fecharecolectaf) 
+  ),
+  fecharecolectaf = case_when(
+    str_detect(fecharecolectaf, "-1") ~ NA,
+    TRUE ~ as.character(fecharecolectaf) 
+  )
+)
+
 
 # Asignación de tipo de columna
 
 base <- base %>%
   mutate(across(
-    c(profundidad, secchi, salinidad, oxigeno,
-      conduc, conduc_abs, resist, PPWO, presion, sat_oxigen, ph,
-      solidos_dis, latitud, longitud, fosfatos, silicatos, amonio,
-      nitritos, nitratos, filtros_chla, chla_agua, faopigmentos,
-      filtros_matsuspension, matsuspension, alcali_parcial, alcali_total,
-      carbonatos, zinc, cobre, ca2, mg2, na, k, cl, SO42, dbof, dboj),
+    c(latitudf, latitudj, longitudf, longitudj,
+      profundidad, salinidad, oxigeno, solid_dis, conduc, resist, presion, sat_oxigen,
+      tempaguaf, tempairef, colifecalf, ecolif, enterococof,
+      tempaguaj, tempairej, colifecalj, ecolij, enterococoj,
+      ph, fosfatos, silicatos, amonio, nitritos, nitratos, chla_agua, faopigmentos, matsuspension, 
+      alcali_total, dureza, carbonatos,
+      zinc, cobre, plomo, cadmio, arsenico, manganeso, ca2, mg2, na, k, cl, SO42, 
+      dbof, dboj, dqof, dqoj
+    ),
     as.numeric
-  ))
+  )
+)
+
+
+base <- base %>% mutate(across(
+    c(sitio, ubi_muestra, cuerpo),
+    as.factor
+))
+
+base$fecha
+
+
+base <- base %>%
+  mutate(
+    # Convertir fechas en formato dd/mm/aaaa
+    across(c(fecharecolectaf, fecharecolectaj),
+           ~ as.Date(., format = "%d/%m/%Y")),
+    
+    # Opcional: Forzar NA para fechas inválidas (ej: meses >12)
+    across(c(fecharecolectaf, fecharecolectaj),
+           ~ ifelse(is.na(as.Date(., format = "%d/%m/%Y")), 
+                    NA, 
+                    as.Date(., format = "%d/%m/%Y")))
+  )
 
 # Asignación de NA's por columna
+base <- base %>% mutate(
+  across(c(
+    latitudf, latitudj, longitudf, longitudj,
+    profundidad, salinidad, oxigeno, solid_dis, conduc, resist, presion, sat_oxigen,
+    tempaguaf, tempairef, colifecalf, ecolif, enterococof,
+    tempaguaj, tempairej, colifecalj, ecolij, enterococoj,
+    ph, fosfatos, silicatos, amonio, nitritos, nitratos, chla_agua, faopigmentos, matsuspension, 
+    alcali_total, dureza, carbonatos,
+    zinc, cobre, plomo, cadmio, arsenico, manganeso, ca2, mg2, na, k, cl, SO42, 
+    dbof, dboj, dqof, dqoj
+  ), 
+  ~na_if(., 1))
+)
 
-base <- base %>%
-  mutate(
-    profundidad = na_if(profundidad, -1),
-    secchi = na_if(secchi, -1),
-    salinidad = na_if(salinidad, -1),
-    oxigeno = na_if(oxigeno, -1),
-    conduc = na_if(conduc, -1),
-    conduc_abs = na_if(conduc_abs, -1),
-    resist = na_if(resist, -1),
-    PPWO = na_if(PPWO, -1),
-    presion = na_if(presion, -1),
-    sat_oxigen = na_if(sat_oxigen, -1),
-    ph = na_if(ph, -1),
-    solidos_dis = na_if(solidos_dis, -1),
-    latitud = na_if(latitud, -1),
-    longitud = na_if(longitud, -1),
-    fosfatos = na_if(fosfatos, -1),
-    silicatos = na_if(silicatos, -1),
-    amonio = na_if(amonio, -1),
-    nitritos = na_if(nitritos, -1),
-    nitratos = na_if(nitratos, -1),
-    filtros_chla = na_if(filtros_chla, -1),
-    chla_agua = na_if(chla_agua, -1),
-    faopigmentos = na_if(faopigmentos, -1),
-    filtros_matsuspension = na_if(filtros_matsuspension, -1),
-    matsuspension = na_if(matsuspension, -1),
-    alcali_parcial = na_if(alcali_parcial, -1),
-    alcali_total = na_if(alcali_total, -1),
-    carbonatos = na_if(carbonatos, -1),
-    zinc = na_if(zinc, -1),
-    cobre = na_if(cobre, -1),
-    ca2 = na_if(ca2, -1),
-    mg2 = na_if(mg2, -1),
-    na = na_if(na, -1),
-    k = na_if(k, -1),
-    cl = na_if(cl, -1),
-    SO42 = na_if(SO42, -1),
-    dbof = na_if(dbof, -1),
-    dboj = na_if(dboj, -1),
-    temperatura = na_if(temperatura, -1),
-    temperatura = na_if(temperatura, 9999.0),
-    longitud = na_if(longitud, 999999.00000),
-    ph1 = na_if(ph1, 9999.00),
-    faopigmentos = na_if(faopigmentos, 9999.00),
-    dboj = na_if(dboj, 999999.0),
-    dbof = na_if(dbof, 888888.0),
-    latitud = na_if(latitud, 9.999990e+05),
-    dqof = na_if(dqof, -1),
-    dqoj = na_if(dqoj, -1),
-    dboj = na_if(dboj, 999999.0),
-    dqoj = na_if(dqoj, 999999.0),
-    dbof = na_if(dboj, 888888),
-    dqof = na_if(dqof, 888888)
-  )
 
-# Arreglar las fechas
-
-base <- base %>%
-  mutate(
-    fecha = as.character(fecha),
-    fecha = na_if(fecha, "-1"),
-    fecha = na_if(fecha, "99/99/99"),
-    fecha = trimws(fecha),
-    fecha = if_else(grepl("^[0-9]{2}[a-z]{3}[0-9]{4}$", fecha),
-                    paste0(substr(fecha, 1, 2), " ", substr(fecha, 3, 5), " ", substr(fecha, 6, 9)),
-                    fecha),
-    fecha = parse_date_time(fecha, orders = c("mdy", "d b Y"), locale = "en_US"),
-    
-    fecha = as.Date(fecha)
-  )
-
-base <- base %>% 
-  mutate(hora = ifelse(hora %in% c("99:99", "99:99:99", "-1"), NA, hora))
-
-base <- base %>% 
-  mutate(hora = ifelse(str_detect(hora, "^\\d{1,2}:\\d{2}$"),
-                       paste0(hora, ":00"),
-                       hora))
-
-base$hora <- as_hms(base$hora)
 
 # Eliminación de columnas
 
