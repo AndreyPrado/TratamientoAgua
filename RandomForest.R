24f8201921ad4c6c5b466a5f1049427670a7963e
Andrey Prado <joseandrey.prado@ucr.ac.cr>
2025-06-25 23:45:57 -0600

    feat: añade pruebas de hipótesis

--- RandomForest.R
+++ RandomForest.R
@@ -4,13 +4,12 @@ library(tidyverse)
 
 base <- read_excel("data/base_agua_limpia.xlsx")
 view(base)
+glimpse(base)
 
 # Extra: volver a poner las categóricas como factores y la fecha como Date por que
 # al pasarse a excel pierde esta característica.
 
 base$sitio <- as.factor(base$sitio)
-base$cuerpo <- as.factor(base$cuerpo)
 base$ubi_muestra <- as.factor(base$ubi_muestra)
-base$analisis_agua1 <- as.factor(base$analisis_agua1)
-base$fecha <- as.Date(base$fecha)
-base$hora <- as_hms(base$hora)
+base$cuerpo <- as.factor(base$cuerpo)
+
